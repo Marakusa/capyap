ba7633be8d986516533f8e883efb6a0d9522ef4f
Markus Kannisto <m.kannisto01@gmail.com>
2025-08-29 07:36:43 +0300

    Update installer

--- CapYap/post_build.bat
+++ CapYap/post_build.bat
@@ -1,5 +1,5 @@
 @echo off
 SET workindir=%~2
 IF %workindir:~-1%==\ SET workindir=%workindir:~0,-1%
-"C:\Program Files (x86)\NSIS\makensis.exe" /DICONFILE="%1" /DWORKINGDIR="%workindir%" /DAPP_VERSION="%4" "%3"
+"C:\Program Files (x86)\NSIS\makensis.exe" /DICONFILE="%1" /DWORKINGDIR="%workindir%" /DAPP_VERSION="%4" /DPLATFORM="%5" "%3"
 if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
